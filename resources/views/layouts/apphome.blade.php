4c13a0db1a754d9ab1736db3de98c1fa639c19ff
Daniel Thalmann <daniel@thalmann.li>
2022-10-10 09:47:06 +0200

    update layout

--- resources/views/layouts/apphome.blade.php
+++ resources/views/layouts/apphome.blade.php
@@ -17,20 +17,8 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased bg-gray-100 dark:bg-gray-900">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+        <!-- Page Content -->
+        {{ $slot }}
+        
     </body>
 </html>
